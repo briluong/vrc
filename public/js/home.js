f335a5d5920aab12410033a5346a0269b38d8479
michaelwang1997 <michaelw.wang@mail.utoronto.ca>
2018-07-10 01:08:55 -0400

    finished nav bar and added modals

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,20 @@
 /**
  * Created by MichaelWang on 2018-07-08.
  */
-let backgroundImage = function() {
-    // randomly return a background image
+/*****************************************************************************/
+/* Initialization Handlers */
+/*****************************************************************************/
+$(document).ready(function () {
     var backgroundImages = ['background1.jpg', 'background2.jpg', 'background3.jpg', 'background4.jpg', 'background5.jpg']
     var i = Math.floor(Math.random() * backgroundImages.length)
     document.getElementById("backgroundPicture").src = backgroundImages[i]
-}
+});
 
-backgroundImage()
+/*****************************************************************************/
+/* Event Handlers */
+/*****************************************************************************/
+
+
+/*****************************************************************************/
+/* Function Handlers */
+/*****************************************************************************/
