8a77318f7224931271b0bc228a8831ca0e46dcb5
michaelwang1997 <michaelw.wang@mail.utoronto.ca>
2018-07-14 03:09:55 -0400

    fixed background picture blob error

--- public/js/home.js
+++ public/js/home.js
@@ -7,7 +7,7 @@
 $(document).ready(function () {
     var backgroundImages = ['background1.jpg', 'background2.jpg', 'background3.jpg', 'background4.jpg', 'background5.jpg']
     var i = Math.floor(Math.random() * backgroundImages.length)
-    document.getElementById("backgroundPicture").src = backgroundImages[i]
+    document.getElementById("backgroundPicture").src = "/" + backgroundImages[i]
 });
 
 /*****************************************************************************/
